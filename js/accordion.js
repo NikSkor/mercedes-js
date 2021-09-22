8c4261cc0c8e76b9d91061eedc9f663e4875132b
Nikita Skorodumov <mr-zerx@yandex.ru>
2021-09-23 00:54:13 +0500

    accordion

--- js/accordion.js
+++ js/accordion.js
@@ -1,4 +1,24 @@
 const btn = document.querySelector('.feature__link');
 const btns = document.querySelectorAll('.feature__link');
+const lists = document.querySelectorAll('.feature-sub');
 
-console.dir(btn);
+btns.forEach((btnItem, index)=>{
+    btnItem.addEventListener('click', ()=> {
+        if (btnItem.classList.contains('feature__link_active')) {
+            btns.forEach((btnItem)=> {
+                btnItem.classList.remove('feature__link_active');
+            })
+            lists[index].classList.add('hidden');
+        } else {
+            btns.forEach((btnItem)=> {
+                btnItem.classList.remove('feature__link_active');
+            })
+            btnItem.classList.add('feature__link_active');
+            lists.forEach((listItem)=> {
+                listItem.classList.add('hidden');
+            })
+
+            lists[index].classList.remove('hidden');
+        }
+    })
+})
